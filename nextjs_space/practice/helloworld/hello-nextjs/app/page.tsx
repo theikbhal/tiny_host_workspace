75f3ab56adf89f968dd6eb9762b2383ca889bd77
Shaik Ikbhal Basha <theikbhal@gmail.com>
2025-11-24 07:18:43 +0530

    hello world next.js 16 lts

---
+++ nextjs_space/practice/helloworld/hello-nextjs/app/page.tsx
@@ -0,0 +1,8 @@
+// app/page.tsx
+export default function Page() {
+  return (
+    <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h1>Hello, Next.js World! 👋</h1>
+    </main>
+  );
+}
